5073eec46598463e8e423d4fba21ee3d0fd1d1a0
lrdnx <lrdnx@icloud.com>
2017-04-28 13:44:03 +0800

    creation
    this is the creation of the project

---
+++ VideoLoadingHud/VideoLoadingHud/VideoLoadingHud.h
@@ -0,0 +1,19 @@
+//
+//  VideoLoadingHud.h
+//  VideoLoadingHud
+//
+//  Created by lrdnx on 2017/4/28.
+//  Copyright © 2017年 DNX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VideoLoadingHud.
+FOUNDATION_EXPORT double VideoLoadingHudVersionNumber;
+
+//! Project version string for VideoLoadingHud.
+FOUNDATION_EXPORT const unsigned char VideoLoadingHudVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideoLoadingHud/PublicHeader.h>
+
+
